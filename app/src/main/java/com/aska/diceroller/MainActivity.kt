5ff773ff2203d79334af4cce652692999d871ffe
Aska2790 <19askaaska90>
2021-02-16 20:45:39 +0200

    Adding button and imageView to the layout Adding button click listener Adding img resources Updating img on button click

--- app/src/main/java/com/aska/diceroller/MainActivity.kt
+++ app/src/main/java/com/aska/diceroller/MainActivity.kt
@@ -2,10 +2,52 @@ package com.aska.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.aska.diceroller.databinding.ActivityMainBinding
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    //region Properties
+
+    private lateinit var binding: ActivityMainBinding
+
+    //endregion
+
+    //region Lifecycle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initialize()
     }
+
+    //endregion
+
+    //region Initialization
+
+    private fun initialize() {
+        binding.buttonDiceRoller.setOnClickListener {
+            onDiceRollerImageUpdateAction()
+        }
+    }
+
+    //endregion
+
+    //region Actions
+
+    private fun onDiceRollerImageUpdateAction(){
+        val nextInt = Random().nextInt(6) + 1
+        val imageResourceId = when (nextInt){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        binding.imageViewDiceRoller.setImageResource(imageResourceId)
+    }
+
+    //endregion
 }
